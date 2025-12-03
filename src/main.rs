6a31b3ba7e4d5f8a433b7e2cbf04a862fca6cd4f
Vasuki Venkatesh <venkateshvasuki@gmail.com>
2025-12-03 16:28:36 -0500

    using env vars for getting db path

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,24 @@ mod db_operations;
 mod errors;
 mod thought;
 
-use clap::Parser;
-
 use crate::config::Args;
 use crate::db_operations::{read, setup_db, write_to_db};
+use clap::Parser;
+use std::env;
 
+fn get_db_path() -> String {
+    env::var("DB_PATH").unwrap_or_else(|_| "thought_app.db".to_string())
+}
 #[cfg(feature = "writer")]
 fn main() -> Result<(), errors::AppError> {
     let args = Args::try_parse()?;
-    let conn = setup_db("my_database.db")?;
+    let conn = setup_db(&get_db_path())?;
     write_to_db(&conn, &args)
 }
 
 #[cfg(feature = "reader")]
 fn main() -> Result<(), errors::AppError> {
-    let conn = setup_db("my_database.db")?;
+    let conn = setup_db(&get_db_path())?;
     let res = read(&conn)?;
     res.iter().for_each(|thought| println!("{:?}", thought));
     Ok(())
